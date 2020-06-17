217c9ebc17c687469782474d32bf87afea5039b6
adam clark <adamclark44@gmail.com>
2020-06-17 18:48:54 -0500

    add my js 5 questions

--- js/apps.js
+++ js/apps.js
@@ -10,10 +10,35 @@ console.log(userName);
 //Greet the user
 alert('Hello ' + userName);
 
+//convert the input to lowerCase
+var lowerCaseName = userName.toLowerCase();
+console.log(lowerCaseName);
+
+//Validate the input using conditional logic using strict equals.
+if(lowerCaseName === 'craig' || lowerCaseName === 'bob') {
+  alert('Welcom back to my page, ' + userName);
+} else {
+  alert('Welcome new user to my page.');
+}
+
+
 //this is where my magic starts
 //first question
-var food = prompt('Do I have a strict plant based diet');
+var food = prompt('Do I have a strict plant based diet? Please answer yes or no');
 console.log(food);
+
+//convert the input to lowerCase
+var lowerCasefood = food.toLowerCase();
+console.log(lowerCasefood);
+
+//Validate the input using conditional logic using strict equals.
+if(lowerCasefood === 'yes') {
+   alert('sorry, this guy is a meat eater!!!');
+} else {
+  alert('That is correct!! I have been exclusively I.F. Carnivore since January 2020! I will take my steak with a side of steak');
+}
+
+
 
 //second question
 var married = prompt('how many times have I been married');
