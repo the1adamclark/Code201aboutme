5b58bc0755e8ebf5b58c644c9e1c3d87a099a01b
adam clark <adamclark44@gmail.com>
2020-06-17 11:36:04 -0500

    add js username

---
+++ js/apps2.js
@@ -0,0 +1,11 @@
+'use strict'
+console.log('this file is connected');
+
+//global variables
+var userPoints = 0;
+
+alert('Hello and welcome to the guessing game.');
+
+var user = prompt('what is your name?');
+var userName = user.toLowerCase();
+console.log(userName);
