6161e6d5b5fe76b36a1393c6da5309f7207f4217
adam clark <adamclark44@gmail.com>
2020-06-16 22:32:18 -0500

    changed notes

--- js/apps.js
+++ js/apps.js
@@ -4,22 +4,22 @@ console.log('This file is connected.');
 
 
 //prompt our user for input data
-// var userName = prompt('What is your name?');
-// console.log(userName);
+var userName = prompt('What is my name?');
+console.log(userName);
 
 //Greet the user
-// alert('Hello ' + userName);
+alert('Hello ' + userName);
 
-// //convert the input to lowerCase
-// var lowerCaseName = userName.toLowerCase();
-// console.log(lowerCaseName);
+//convert the input to lowerCase
+var lowerCaseName = userName.toLowerCase();
+console.log(lowerCaseName);
 
-// //Validate the input using conditional logic using strict equals.
-// if(lowerCaseName === 'craig' || lowerCaseName === 'bob') {
-//   alert('Welcom back to my page, ' + userName);
-// } else {
-//   alert('Welcome new user to my page.');
-// }
+//Validate the input using conditional logic using strict equals.
+if(lowerCaseName === 'craig' || lowerCaseName === 'bob') {
+  alert('Welcom back to my page, ' + userName);
+} else {
+  alert('Welcome new user to my page.');
+}
 
 
 // logical operator example
