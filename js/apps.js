8a7225361e3863ce3f0fd7264cfe27aa6444a80c
adam clark <adamclark44@gmail.com>
2020-06-18 09:59:50 -0500

    added my technical requirements to my code as commented out so i can follow it better

--- js/apps.js
+++ js/apps.js
@@ -187,3 +187,26 @@ if(lowerCasework === 'yes' || lowerCasework === 'yep' || 'yeah') {
 // // Make sure that all code is cleanly written with correct indentation and syntax. Look at examples online and in your textbook. Note that your JS must pass the rules in the linter that we installed.
 
 // // Follow the Add, Commit, Push process. It should be evident in GitHub that commits are made regularly, and with good commit messages that explain the WHY of the commit.
+
+
+
+// lab03 extend your guessing game
+
+// 1. Create a “Top Ten” at the bottom of your HTML page as an ordered list in HTML. 
+//     Some ideas that you can include could be top ten movies, top ten favorite places, or top ten places to visit. 
+//     You may choose to do whatever top ten list that you wish.
+//     Convert your work experience and education summary into an unordered list using HTML
+
+//2.Add a 6th question to the guessing game that takes in a numeric input by prompting the user to guess a number.
+//    Indicates through an alert if the guess is “too high” or “too low”.
+//    It should give the user exactly four opportunities to get the correct answer.
+//    After all attempts have been exhausted, tell the user the correct answer. Consider using a loop of some sort.
+
+//3.Add a 7th question that has multiple possible correct answers that are stored in an array.
+//    Give the user 6 attempts to guess the correct answer.
+//    The guesses will end once the user guesses a correct answer or they run out of attempts.
+//    Display all the possible correct answers to the user.
+//    Consider using a loop of some sort for this question.
+
+//4.Keep track of the total number of correct answers. 
+//At the end tell them how many they got correct out of the 7 questions asked.
